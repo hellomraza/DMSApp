51d0390a944b3ecd7b5c13056531bef16fea60da
hellomraza <hellomraza@gmail.com>
2025-09-11 22:25:54 +0530

    configure @tanstack/react-query and Redux with PersistGate for API calls and state management

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Navigation from './src/navigation/Navigation';
+import { persistor, store } from './src/store';
+
+const queryClient = new QueryClient();
+
+const LoadingView = () => (
+  <View style={styles.loadingContainer}>
+    <ActivityIndicator size="large" />
+  </View>
+);
 
 const App = () => {
-  return <Navigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={<LoadingView />} persistor={persistor}>
+          <Navigation />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
+  );
 };
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
