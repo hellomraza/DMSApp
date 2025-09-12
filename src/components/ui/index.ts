e38f7d2a47bdfac5aac287ad207c45fee0ee12cd
hellomraza <hellomraza@gmail.com>
2025-09-12 18:02:24 +0530

    feat: export FileSearchComponent in UI index for improved component accessibility

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as CustomButton } from './CustomButton';
 export { default as CustomTextInput } from './CustomTextInput';
+export { default as FileSearchComponent } from './FileSearchComponent';
 export { default as FileUploadComponent } from './FileUploadComponent';
