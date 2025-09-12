cf79b17dfc9d1289d12535539b70ad1577111421
hellomraza <hellomraza@gmail.com>
2025-09-12 18:39:00 +0530

    feat: reorganize exports in index.ts for better structure and clarity

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,10 @@ export { default as CustomButton } from './CustomButton';
 export { default as CustomTextInput } from './CustomTextInput';
 export { default as FileSearchComponent } from './FileSearchComponent';
 export { default as FileUploadComponent } from './FileUploadComponent';
+
+// File Search Component Parts
+export { default as DocumentCard } from './DocumentCard';
+export { default as DocumentList } from './DocumentList';
+export { default as DocumentPreviewModal } from './DocumentPreviewModal';
+export { default as SearchFilters } from './SearchFilters';
+export { default as SearchHeader } from './SearchHeader';
